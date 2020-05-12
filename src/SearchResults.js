92e548fca30066a8f28513b5db54468851ff97e6
Matt Keane <mattpkeane@gmail.com>
2020-05-12 14:46:51 -0500

    displays loader while awaiting response

---
+++ src/SearchResults.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function SearchResults(props) {
+	return(
+		<React.Fragment>
+			Results go here.
+		</React.Fragment>
+	)
+}
--- src/SearchView.js
+++ src/SearchView.js
@@ -1,15 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import SearchForm from "./SearchForm"
+import SearchResults from "./SearchResults"
+import { Loader } from "semantic-ui-react"
 
 export default function SearchView() {
+	const [loading, setLoading] = useState(false)
 	const getRecipes = async (searchTerm) => {
 		try {
+			setLoading(true)
 			const url = process.env.REACT_APP_API_URL + "/api/v1/recipes/search/"
 			console.log("calling getRecipes")
 			const everySpace = new RegExp(/\s/, "g")
 			const apiSearchTerm = searchTerm.replace(everySpace, "+")
 			const searchResponse = await fetch(url + apiSearchTerm)
 			console.log(searchResponse.json())
+			setLoading(false)
 		} catch (err) {
 			console.log(err)
 		}
@@ -19,6 +24,15 @@ export default function SearchView() {
 			<SearchForm
 				getRecipes={getRecipes}
 			/>
+			{
+				loading
+				?
+				<Loader active>
+					Hopping to your recipes
+				</Loader>
+				:
+				<SearchResults />
+			}
 		</React.Fragment>
 		)
 }
